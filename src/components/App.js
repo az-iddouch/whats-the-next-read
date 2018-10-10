d635a1f4ad8cba2698dcb153c8c7574b46ad9c29
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-10-10 19:41:52 +0100

    my initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from './partials/Header';
+import './App.sass';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
